d61828b2f4288320542a386b4cfd2918ed94ac77
Joody20 <loveed123@naver.com>
2026-01-21 13:15:37 +0900

    refactor: 컴포넌트 경로 수정

--- app/signup/step2/page.tsx
+++ app/signup/step2/page.tsx
@@ -1,4 +1,4 @@
-import SignupStep2 from "@/src/features/signup/components/SignupStep2";
+import SignupStep2 from "@/src/features/signup/components/SignupStep2/SignupStep2Page";
 
 export default function Page() {
   return <SignupStep2 />;
