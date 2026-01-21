9678044f57c80bdd96b0ce83f9579d5de7461c93
Joody20 <loveed123@naver.com>
2026-01-21 14:27:39 +0900

    style: 스타일 코드 수정

--- src/features/signup/components/SignupStep2/BodyInfoSection.tsx
+++ src/features/signup/components/SignupStep2/BodyInfoSection.tsx
@@ -19,37 +19,35 @@ const BodyInfoSection = memo(
     onWeightChange,
     weightInputRef,
   }: Props) => (
-    <div className="mt-8 flex gap-4">
-      <div className="flex-1">
+    <div className="mt-10 flex items-center justify-center gap-8">
+      <div>
         <label className="mb-1 block text-[13px] font-medium">키</label>
-        <div className="relative">
+        <div className="inline-flex items-center gap-2">
           <Input
             value={heightValue}
             inputMode="numeric"
             pattern="[0-9]*"
             placeholder="예: 176"
+            className="text-right text-[12px] placeholder:text-right placeholder:text-[13px] placeholder:text-[#d9d9d9]"
             onChange={(e) => onHeightChange(e.target.value)}
           />
-          <span className="absolute right-2 top-1/2 -translate-y-1/2 text-sm">
-            cm
-          </span>
+          <span className="text-sm text-muted-foreground">cm</span>
         </div>
       </div>
 
-      <div className="flex-1">
+      <div>
         <label className="mb-1 block text-[13px] font-medium">몸무게</label>
-        <div className="relative">
+        <div className="inline-flex items-center gap-2">
           <Input
             ref={weightInputRef}
             value={weightValue}
             inputMode="numeric"
             pattern="[0-9]*"
             placeholder="예: 68"
+            className="w-full  text-right text-[13px] placeholder:text-[13px] placeholder:text-[#d9d9d9]"
             onChange={(e) => onWeightChange(e.target.value)}
           />
-          <span className="absolute right-2 top-1/2 -translate-y-1/2 text-sm">
-            kg
-          </span>
+          <span className="text-sm text-muted-foreground">kg</span>
         </div>
       </div>
     </div>
