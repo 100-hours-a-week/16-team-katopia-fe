077fae5b92ba4e6b5ab50a2c5e4583b16e428d01
Joody20 <loveed123@naver.com>
2026-01-21 13:16:16 +0900

    refactor: AppHeader 수정

--- src/features/home/components/HomePage.tsx
+++ src/features/home/components/HomePage.tsx
@@ -1,21 +1,16 @@
-// src/features/home/components/HomePage.tsx
 "use client";
 
-import HomeHeader from "./HomeHeader";
+import AppHeader from "@/src/shared/components/layout/AppHeader";
 import LoginBottomSheet from "./LoginBottomsSheet";
 
 export default function HomePage() {
   return (
     <div className="relative min-h-screen flex flex-col">
-      {/* 상단 영역 */}
-      <HomeHeader />
-
-      {/* 메인 콘텐츠 (지금은 비워둠) */}
+      <AppHeader />
       <main className="flex-1 pt-14 flex items-center justify-center">
         <p className="text-center">다음 버전에서 만나요 ! :) </p>
       </main>
 
-      {/* 하단 로그인 바텀시트 */}
       <LoginBottomSheet />
     </div>
   );
