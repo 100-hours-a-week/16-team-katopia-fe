febb4262b34daf5b67de4b97b03f9e6df1a40074
Joody20 <loveed123@naver.com>
2026-01-22 20:43:32 +0900

    feat: api 분리

---
+++ src/config/api.ts
@@ -0,0 +1,5 @@
+export const API_BASE_URL = "http://localhost:8080";
+
+export const KAKAO_OAUTH_URL =
+  process.env.NEXT_PUBLIC_KAKAO_OAUTH_URL ||
+  "http://localhost:8080/oauth2/authorization/kakao";
