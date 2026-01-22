a591b8696dd7d59a4e59855300b6c0c064c08ff9
Joody20 <loveed123@naver.com>
2026-01-22 20:47:15 +0900

    feat: AT 발급, 로그인 바텀시트 로그인/비로그인 시 노출 로직

--- src/features/home/components/HomePage.tsx
+++ src/features/home/components/HomePage.tsx
@@ -2,8 +2,14 @@
 
 import AppHeader from "@/src/shared/components/layout/AppHeader";
 import LoginBottomSheet from "./LoginBottomsSheet";
+import { getAccessToken } from "@/src/lib/auth";
+import { useSearchParams } from "next/navigation";
 
 export default function HomePage() {
+  const searchParams = useSearchParams();
+  const isLoggedIn = !!getAccessToken();
+  const isActiveState = searchParams.get("STATE") === "ACTIVE";
+
   return (
     <div className="relative min-h-screen flex flex-col">
       <AppHeader />
@@ -11,7 +17,7 @@ export default function HomePage() {
         <p className="text-center">다음 버전에서 만나요 ! :) </p>
       </main>
 
-      <LoginBottomSheet />
+      {!isLoggedIn && !isActiveState && <LoginBottomSheet />}
     </div>
   );
 }
