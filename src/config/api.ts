fa6cb5b2bae42bbc086e4fb530e4cb774a214e22
Joody20 <loveed123@naver.com>
2026-01-23 22:43:47 +0900

    refactor: api 주소 변경

--- src/config/api.ts
+++ src/config/api.ts
@@ -5,12 +5,3 @@ export const API_BASE_URL = "https://dev.fitcheck.kr";
 export const KAKAO_OAUTH_URL =
   process.env.NEXT_PUBLIC_KAKAO_OAUTH_URL ||
   "https://dev.fitcheck.kr/oauth2/authorization/kakao";
-
-// // const profile = 'local';
-// const profile = 'dev';
-
-// if (profile === 'local') {
-//   // localhost 환경에서만 동작하는 코드
-// } else if (profile === 'dev') {
-//   // dev 환경에서만 동작하는 코드
-// }
