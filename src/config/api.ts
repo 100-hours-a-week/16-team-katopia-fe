af5f092e316f5b65ccf575678514a5461cebe82a
Joody20 <loveed123@naver.com>
2026-01-30 16:32:14 +0900

    fix: 경로 수정

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,2 @@
-export const API_BASE_URL = "https://dev.fitcheck.kr";
-export const KAKAO_OAUTH_URL =
-  "https://dev.fitcheck.kr/oauth2/authorization/kakao";
+export const API_BASE_URL = "https://fitcheck.kr";
+export const KAKAO_OAUTH_URL = "https://fitcheck.kr/oauth2/authorization/kakao";
