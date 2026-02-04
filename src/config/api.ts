aa5c3341314d1245a001f3dec9339a5bf66e42c8
Joody20 <loveed123@naver.com>
2026-02-04 13:23:41 +0900

    fix: url 수정

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,6 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://dev.fitcheck.kr";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://fitcheck.kr";
 export const IMAGE_BASE_URL =
   process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
-  "https://df1ez4kkj7703.cloudfront.net";
-export const KAKAO_OAUTH_URL =
-  "https://dev.fitcheck.kr/oauth2/authorization/kakao";
+  "http://dygyjag00bi04.cloudfront.net/";
+export const KAKAO_OAUTH_URL = "https://fitcheck.kr/oauth2/authorization/kakao";
