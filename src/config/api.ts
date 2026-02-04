838933a929ee36e8c3fd40544b161ff8bc210a74
Joody20 <loveed123@naver.com>
2026-02-04 10:55:53 +0900

    fix: url 오타 수정

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,6 +2,6 @@ export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://dev.fitcheck.kr";
 export const IMAGE_BASE_URL =
   process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
-  "https://df1ez4kkj7703.cloudfront.net ";
+  "https://df1ez4kkj7703.cloudfront.net";
 export const KAKAO_OAUTH_URL =
   "https://dev.fitcheck.kr/oauth2/authorization/kakao";
