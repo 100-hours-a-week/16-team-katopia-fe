e9fa1f56140f14d48d9f1a34d5809675c7a74634
brixxt27 <brixxt27@gmail.com>
2026-02-05 14:22:15 +0900

    chore: 개발 환경 분리 테스트
    - prod, dev의 base url과 image base url을 변경하였습니다.
    - src/config/api.ts 파일의 기본값을 삭제했습니다. 그렇게하여 ci/cd yaml의 env 블록 테스트 환경을 구성했습니다.

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,3 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://dev.fitcheck.kr";
-export const IMAGE_BASE_URL =
-  process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
-  "https://df1ez4kkj7703.cloudfront.net";
-export const KAKAO_OAUTH_URL =
-  "https://dev.fitcheck.kr/oauth2/authorization/kakao";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
+export const KAKAO_OAUTH_URL = `${API_BASE_URL}/oauth2/authorization/kakao`;
