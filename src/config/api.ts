f48516782a02fe1362c6c363787e29393fc27d2a
brixxt27 <brixxt27@gmail.com>
2026-02-05 14:44:27 +0900

    chore: 테스트 용으로 base url을 naver 도메인으로 변경

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://dev.fitcheck.kr";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://naver.com";
 export const IMAGE_BASE_URL =
   process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
   "https://df1ez4kkj7703.cloudfront.net";
