7dc2e33b8479a96ea53cc970adbc22e6dd931d94
brixxt27 <brixxt27@gmail.com>
2026-02-05 14:42:38 +0900

    chore: 빌드 시 컴파일 -> 타입 체크 과정을 거치므로 런타임에 필요한 기본값 정의

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,6 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL;
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://dev.fitcheck.kr";
+export const IMAGE_BASE_URL =
+  process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
+  "https://df1ez4kkj7703.cloudfront.net";
 export const KAKAO_OAUTH_URL = `${API_BASE_URL}/oauth2/authorization/kakao`;
