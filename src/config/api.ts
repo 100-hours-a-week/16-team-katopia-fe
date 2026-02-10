df93eeccd9ddb55d35f48994b5b9691d916c23f6
sh123456-boop <hash04@naver.com>
2026-02-10 12:59:50 +0900

    api.ts prettier 수정

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,3 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? 
-  "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com";
-export const IMAGE_BASE_URL =
-  process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
-  "http://dygyjag00bi04.cloudfront.net/";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com";
+export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL ?? "http://dygyjag00bi04.cloudfront.net/";
 export const KAKAO_OAUTH_URL = "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com/oauth2/authorization/kakao";
