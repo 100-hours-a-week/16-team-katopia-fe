cc1b71b66c172c8ee08ef8830dbd9427854e105e
sh123456-boop <hash04@naver.com>
2026-02-10 13:04:48 +0900

    ecr tag test

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,8 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com";
-export const IMAGE_BASE_URL = process.env.NEXT_PUBLIC_IMAGE_BASE_URL ?? "http://dygyjag00bi04.cloudfront.net/";
-export const KAKAO_OAUTH_URL = "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com/oauth2/authorization/kakao";
+export const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL ??
+  "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com";
+export const IMAGE_BASE_URL =
+  process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
+  "http://dygyjag00bi04.cloudfront.net/";
+export const KAKAO_OAUTH_URL =
+  "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com/oauth2/authorization/kakao";
