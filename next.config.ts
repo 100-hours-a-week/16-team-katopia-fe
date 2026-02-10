1447386ff015f97f6510d511f60587c580135335
sh123456-boop <hash04@naver.com>
2026-02-10 12:54:54 +0900

    docker cicd test

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
       // 기존 테스트용
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,7 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://fitcheck.kr";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? 
+  "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com";
 export const IMAGE_BASE_URL =
   process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
   "http://dygyjag00bi04.cloudfront.net/";
-export const KAKAO_OAUTH_URL = "https://fitcheck.kr/oauth2/authorization/kakao";
+export const KAKAO_OAUTH_URL = "https://katopia-docker-alb-188390849.ap-northeast-2.elb.amazonaws.com/oauth2/authorization/kakao";
