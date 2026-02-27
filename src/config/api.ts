5d9b2be06a807cb90981396233e3a867e075dd58
sh123456-boop <hash04@naver.com>
2026-02-27 16:56:19 +0900

    cicd 변경

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://dev.fitcheck.kr";
+  process.env.NEXT_PUBLIC_API_BASE_URL ?? "https://fitcheck.kr";
 export const IMAGE_BASE_URL =
   process.env.NEXT_PUBLIC_IMAGE_BASE_URL ??
   "http://dygyjag00bi04.cloudfront.net/";
 export const KAKAO_OAUTH_URL =
-  "https://dev.fitcheck.kr/oauth2/authorization/kakao";
+  "https://fitcheck.kr/oauth2/authorization/kakao";
