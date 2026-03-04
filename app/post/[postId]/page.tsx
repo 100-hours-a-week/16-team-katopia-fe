dc5ee4ed43d03e526bebfe05c2cc32f9a0a7a42a
Joody20 <loveed123@naver.com>
2026-03-04 10:18:58 +0900

    fix: ISR 로직 수정

--- app/post/[postId]/page.tsx
+++ app/post/[postId]/page.tsx
@@ -2,9 +2,15 @@ import PostDetailPage from "@/src/features/post/components/PostDetailPage";
 import { getPostDetailServer } from "@/src/features/post/api/getPostDetailServer";
 import { notFound } from "next/navigation";
 
+export const revalidate = 3600;
+
 type Props = {
   params: Promise<{ postId: string }>;
 };
+
+export async function generateStaticParams(): Promise<{ postId: string }[]> {
+  return [];
+}
 
 export default async function Page({ params }: Props) {
   const { postId } = await params;
